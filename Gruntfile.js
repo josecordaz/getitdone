454ea583d02ecb85de1bb5804e3ece4f8237ae98
Jose Carlos <sonykarl@gmail.com>
2016-04-24 16:45:12 -0700

    menu and the first view

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,40 +1,13 @@
 'use strict';
 
 module.exports = function(grunt){
+	//require('time-grunt')(grunt);
 
 	require('jit-grunt')(grunt,{
 		useminPrepare: 'grunt-usemin'
 	});
 
 	grunt.initConfig({
-		watch : {
-	  		livereload: {
-	            options: {
-	                livereload: '<%= connect.options.livereload %>'
-	            },
-	            files: [
-	                'dist/{,*/}*.html'
-	            ]
-	      	}
-	  	},
-		connect: {
-	      options: {
-	        port: 9001,
-	        hostname: 'localhost',
-	        livereload: 35729
-	      },
-	      prod: {
-	        options: {
-	          open: true,
-	          base:{
-           		path: 'dist',
-	            options: {
-	                index: 'index.html'
-	            }
-	          }
-	        }
-	      }
-	    },
 	    clean: {
 	        build:{
 	            src: [ 'dist/']
@@ -55,34 +28,83 @@ module.exports = function(grunt){
 	        dist: {}
 	    },
 	      // Uglify
-	    uglify: {
+	    /*uglify: {
 	        // dist configuration is provided by useminPrepare
+	        dist: {},
+	        compress:{
+	        	unused : false
+	        }
+	    },*/
+	    /*cssmin: {
 	        dist: {}
-	    },
-	    cssmin: {
-	        dist: {}
-	    },
+	    },*/
 	    usemin: {
 	        html: ['dist/*.html'],
 	        css: ['dist/styles/*.css'],
 	        options: {
 	            assetsDirs: ['dist', 'dist/styles']
 	        }
-	    },copy: {
+	    },
+	    copy: {
 	      dist: {
 	        cwd: 'app',
 	        src: [ '**','!styles/**/*.css','!scripts/**/*.js' ],
 	        dest: 'dist',
 	        expand: true
 	      }
-	  	}
+	  	},
+	  	watch : {
+			//copy: {
+	        //    files: [ 'app/**', '!app/**/*.css', '!app/**/*.js'],
+	        //    tasks: [ 'build' ]
+	        //},
+	        //scripts: {
+	        //    files: ['app/scripts/app.js'],
+	        //    tasks:[ 'build']
+	        //},
+	        //styles: {
+	        //    files: ['app/styles/mystyles.css'],
+	        //    tasks:['build']
+	        //},
+	  		livereload: {
+	            options: {
+	                livereload: '<%= connect.options.livereload %>'
+	            },
+	            files: [
+	                'app/{,*/}*.html',
+	                '.tmp/styles/{,*/}*.css'
+	            ]
+	      	}
+	  	},
+		connect: {
+	      options: {
+	        port: 9001,
+	        hostname: 'localhost',
+	        livereload: 35729
+	      },
+	      prod: {
+	        options: {
+	          open: true,
+	          base:{
+           		path: '.',
+	            options: {
+	                index: 'index.html'
+	            }
+	          }
+	        }
+	      }
+	    }
 	});
 	grunt.registerTask('build',[
 		'clean',
 		'useminPrepare',
 		'concat',
-		'cssmin',
-		'copy'
+		//'cssmin',
+		//'uglify',
+		'copy',
+		'usemin'
 	]);
-	grunt.registerTask('default',['connect','watch']);
+	grunt.registerTask('prod',['build','connect:prod','watch']);
+
+	grunt.registerTask('default',['connect:prod','watch']);
 };
